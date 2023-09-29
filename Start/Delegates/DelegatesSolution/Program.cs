44b29b9373b29d096e23a692f0e974409a9b8ea6
Ruth Waiganjo <ruthwaiganjo@Ruths-MacBook-Pro.local>
2023-09-29 07:35:50 +0300

    delegates solution, kicked me in the butt

--- Start/Delegates/DelegatesSolution/Program.cs
+++ Start/Delegates/DelegatesSolution/Program.cs
@@ -6,6 +6,43 @@ namespace DelegatesSolution
     {
         static void Main(string[] args)
         {
+            ShippingFeesDelegate theDel;
+            ShippingDestination theDest;
+            string zone;
+
+            do {
+                Console.WriteLine("Enter the shipping zone: ");
+                zone = Console.ReadLine();
+
+                if (!zone.Equals("exit"))
+                {
+                    theDest = ShippingDestination.getDestinationInfo(zone);
+
+                    if(theDest != null)
+                    {
+                        Console.WriteLine("Item Price");
+                        decimal price = decimal.Parse(Console.ReadLine());
+
+                        theDel = theDest.calcFees;
+
+                        if(theDest.m_isHighRisk)
+                        {
+                            theDel += delegate (decimal price, ref decimal fee)
+                            {
+                                fee += 25.0m;
+                            };
+                        }
+
+                        decimal fee = 0.0m;
+                        theDel(price, ref fee);
+                        Console.WriteLine("The shipping fee {0}", fee);
+                    }
+                    else
+                    {
+                        Console.WriteLine("You have entered wrong dest");
+                    }
+                }
+            } while ( zone != "exit" );
         }
     }
 }
